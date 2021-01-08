6de17583241b266dfe6f021ad34d1b5d6559da95
Facundo Chaud <faku_imper@hotmail.com.ar>
2021-01-08 14:51:25 -0300

    Remove unused files

--- divine_office/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DivineOfficeConfig(AppConfig):
-    name = 'divine_office'
--- syllable/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SyllableConfig(AppConfig):
-    name = 'syllable'
